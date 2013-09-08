3e13eaafdc125ea304bc73a6bc35d9560d830d57
RedricShuhart <sergiy.tolok@gmail.com>
2013-09-08 11:41:54 +0300

    Class User was added

---
+++ src/main/java/com/tolok/testerweb/model/User.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tolok.testerweb.model;
+
+/**
+ *
+ * @author Сергей
+ */
+public class User {
+    
+}
